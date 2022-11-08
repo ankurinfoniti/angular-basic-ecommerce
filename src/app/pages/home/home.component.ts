8da61c9997f68ee1d31496ff6815051a70a761eb
Ankur Mishra <ankur.mishra@orange.com>
2022-11-08 10:24:04 +0530

    product page design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROW_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROW_HEIGHT[this.cols];
   category: string | undefined;
 
   constructor() {}
@@ -15,6 +17,7 @@ export class HomeComponent implements OnInit {
 
   oncolumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+    this.rowHeight = ROW_HEIGHT[this.cols];
   }
 
   onShowCategory(newCategory: string): void {
